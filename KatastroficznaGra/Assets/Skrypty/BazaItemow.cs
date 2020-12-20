34b66f2df12fb2293c60d2129708e58e625dc21f
thief01 <pst12345@wp.pl>
2020-12-20 11:36:49 +0100

    Repair misstake

---
+++ KatastroficznaGra/Assets/Skrypty/BazaItemow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BazaItemow : MonoBehaviour
+{
+    public List<Item> items = new List<Item>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*items.Add(new Item(0, "Pusty", "Pusty dla tetow"));
+        items.Add(new Item(1, "Drewno", "Prosty materiał budowlany"));
+        items.Add(new Item(2, "Kamien", "Mocniejszy materiał budowlany"));
+        items.Add(new Item(3, "Jedzenie", "Ogólnie przyjęta strawa"));*/
+    }
+
+}
---
+++ KatastroficznaGra/Assets/Skrypty/ScriptAbleObjecty/BlokRozmowy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//edytowany 18.12.2020
+
+[CreateAssetMenu]
+public class BlokRozmowy : ScriptableObject
+{
+    public string Tekst;
+    public int IDBloku;
+    public List<string> OpcjeWyboru;
+
+    public string PobierzTekst()
+    {
+        return Tekst;
+    }
+}
